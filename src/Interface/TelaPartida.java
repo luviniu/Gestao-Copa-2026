eba88ced09df6a05b25688f6367e200c1cc83a58
Luan Alves <luviviu.alves@gmail.com>
2026-04-29 13:27:08 -0300

    Adição de Tela de Partidas

---
+++ src/Interface/TelaPartida.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import javax.swing.*;
+
+public class TelaPartida {
+    private JPanel panel1;
+    private JButton cadastrarPartidaButton;
+    private JButton editarPartidaButton;
+    private JButton excluirPartidaButton;
+    private JButton listarPartidasButton;
+}
