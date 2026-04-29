03ac81dcac9657c9b418a690402a11366ae47d29
elyeladht <elyel.adht@gmail.com>
2026-04-29 10:46:20 -0300

    Adição das telas de Gestao de Seleções e Elenco

---
+++ src/Interface/TelaGestaoElenco.java
@@ -0,0 +1,18 @@
+package Interface;
+
+import javax.swing.*;
+
+public class TelaGestaoElenco extends JFrame {
+    private JTable table1;
+    private JTextField nomeTextField;
+    private JTextField idadeTextField;
+    private JButton voltarBotao;
+    private JComboBox posicaoComboBox;
+    private JComboBox statusComboBox;
+    private JButton adicionarJogadorBotao;
+    private JPanel painelPrincipal;
+    private JScrollPane tabelaJogadores;
+    private JButton alterarJogadorBotao;
+    private JButton removerJogadorBotao;
+    private JLabel JlabelAdicionarJogador;
+}
---
+++ src/Interface/TelaGestaoSelecoes.java
@@ -0,0 +1,12 @@
+package Interface;
+
+import javax.swing.*;
+
+public class TelaGestaoSelecoes {
+    private JLabel tituloPrinicipal;
+    private JTable table1;
+    private JButton verElencoButton;
+    private JButton alterarInformaçõesDeSeleçãoButton;
+    private JButton removerSeleçãoJButton;
+    private JButton novaSeleçãoButton;
+}
