8fcc1495fe0e0a5231ad20c5c82dc1340bab2f08
Luan Alves <luviviu.alves@gmail.com>
2026-06-15 11:50:41 -0300

    Atualizacao TelaLauncher
    Adiciado layout para todos os Atores e linkando com suas respectivas telas
    
    Falta botar os resultados dos relatorios

--- src/main/java/Interface/TelaLauncher.java
+++ src/main/java/Interface/TelaLauncher.java
@@ -24,7 +24,17 @@ public class TelaLauncher {
     @FXML private AnchorPane painelInicial;
     @FXML private AnchorPane partidaPanelAdm;
     @FXML private AnchorPane desempenhoPanelAdm;
+    @FXML private AnchorPane partidaPanelOrg;
+    @FXML private AnchorPane arbitroPanelOrg;
+    @FXML private AnchorPane selecaoPanelOrg;
+    @FXML private AnchorPane estadioPanelOrg;
+    @FXML private AnchorPane ingressoPanelOp;
+    @FXML private AnchorPane partidaPanelArb;
+
     @FXML private AnchorPane buttonsAdmin;
+    @FXML private AnchorPane buttonsOrganizador;
+    @FXML private AnchorPane buttonsOperador;
+    @FXML private AnchorPane buttonsArbitro;
 
     @FXML private Button btnPerfilInicio;
     @FXML private Button btnPartidasAdm;
@@ -48,11 +58,28 @@ public class TelaLauncher {
 
             String cargo = logado.getPerfilUsuario().toUpperCase();
 
-            if (cargo.equals("ADMINISTRADOR")) {
-                buttonsAdmin.setVisible(true);
+            if (buttonsAdmin != null) buttonsAdmin.setVisible(false);
+            if (buttonsOrganizador != null) buttonsOrganizador.setVisible(false);
+            if (buttonsOperador != null) buttonsOperador.setVisible(false);
+            if (buttonsArbitro != null) buttonsArbitro.setVisible(false);
 
-            } else {
-                buttonsAdmin.setVisible(false);
+            switch (cargo) {
+                case "ADMINISTRADOR":
+                    if (buttonsAdmin != null) buttonsAdmin.setVisible(true);
+                    break;
+                case "ORGANIZADOR":
+                    if (buttonsOrganizador != null) buttonsOrganizador.setVisible(true);
+                    break;
+                case "OPERADOR":
+                    if (buttonsOperador != null) buttonsOperador.setVisible(true);
+                    break;
+                case "ÁRBITRO":
+                case "ARBITRO":
+                    if (buttonsArbitro != null) buttonsArbitro.setVisible(true);
+                    break;
+                default:
+                    System.out.println("Cargo não identificado: " + cargo);
+                    break;
 
             }
 
@@ -64,6 +91,12 @@ public class TelaLauncher {
         if (painelInicial != null) painelInicial.setVisible(false);
         if (partidaPanelAdm != null) partidaPanelAdm.setVisible(false);
         if (desempenhoPanelAdm != null) desempenhoPanelAdm.setVisible(false);
+        if (partidaPanelOrg != null) partidaPanelOrg.setVisible(false);
+        if (arbitroPanelOrg != null) arbitroPanelOrg.setVisible(false);
+        if (selecaoPanelOrg != null) selecaoPanelOrg.setVisible(false);
+        if (estadioPanelOrg != null) estadioPanelOrg.setVisible(false);
+        if (ingressoPanelOp != null) ingressoPanelOp.setVisible(false);
+        if (partidaPanelArb != null) partidaPanelArb.setVisible(false);
 
     }
 
@@ -133,6 +166,158 @@ public class TelaLauncher {
     }
 
     @FXML
+    public void mostrarPainelPartidasOrg(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interface/TelaPartidas.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1920, 1080);
+
+            if (getClass().getResource("/Interface/style.css") != null) {
+                scene.getStylesheets().add(getClass().getResource("/Interface/style.css").toExternalForm());
+
+            }
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setTitle("Gerenciamento de Partidas");
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir a tela de partidas!");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @FXML
+    public void mostrarPainelArbitrosOrg(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interface/TelaArbitros.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1920, 1080);
+
+            if (getClass().getResource("/Interface/style.css") != null) {
+                scene.getStylesheets().add(getClass().getResource("/Interface/style.css").toExternalForm());
+
+            }
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setTitle("Gerenciamento de Árbitros");
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir a tela de árbitros!");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @FXML
+    public void mostrarPainelSelecaoOrg(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interface/TelaSelecoes.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1920, 1080);
+
+            if (getClass().getResource("/Interface/style.css") != null) {
+                scene.getStylesheets().add(getClass().getResource("/Interface/style.css").toExternalForm());
+
+            }
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setTitle("Gerenciamento de Seleções");
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir a tela de seleções!");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @FXML
+    public void mostrarPainelEstadiosOrg(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interface/TelaEstadios.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1920, 1080);
+
+            if (getClass().getResource("/Interface/style.css") != null) {
+                scene.getStylesheets().add(getClass().getResource("/Interface/style.css").toExternalForm());
+
+            }
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setTitle("Gerenciamento de Estádios");
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir a tela de estádios!");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @FXML
+    public void mostrarPainelIngressosOp(ActionEvent event) {
+        esconderTodosOsPaineis();
+        if (ingressoPanelOp != null) {
+            ingressoPanelOp.setVisible(true);
+            ingressoPanelOp.toFront();
+
+        }
+
+    }
+
+    @FXML
+    public void irParaTelaIngressos(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interface/TelaIngressos.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1920, 1080);
+
+            if (getClass().getResource("/Interface/style.css") != null) {
+                scene.getStylesheets().add(getClass().getResource("/Interface/style.css").toExternalForm());
+
+            }
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setTitle("Gerenciamento de Ingressos");
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir a tela de ingressos!");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @FXML
+    public void mostrarPainelPartidasArb(ActionEvent event) {
+        esconderTodosOsPaineis();
+        if (partidaPanelArb != null) {
+            partidaPanelArb.setVisible(true);
+            partidaPanelArb.toFront();
+
+        }
+
+    }
+
+    @FXML
     public void realizarLogout(ActionEvent event) {
         if (janelaGestaoAtiva != null && janelaGestaoAtiva.isShowing()) {
             janelaGestaoAtiva.close();
@@ -141,6 +326,7 @@ public class TelaLauncher {
         }
 
         oprSessao.encerrar();
+
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Interface/TelaLogin.fxml"));
             Parent root = loader.load();
